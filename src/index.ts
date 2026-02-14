e01cb12c9c4842d294dae3505193207dfa4f4589
ErayCetinX <cetin.eray@hotmail.com>
2026-02-14 20:46:13 +0300

    Videos 111-113 are complete

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
+const reader = new CsvFileReader("football.csv");
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => row.split(","));
-
+reader.read();
 enum MatchResult {
   HomeWin = "H",
   AwayWin = "A",
@@ -15,7 +10,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (
     (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
     (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
