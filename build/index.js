4b9e45500099ab714c562d5c03c362da44bcfa20
ErayCetinX <cetin.eray@hotmail.com>
2026-02-18 15:00:10 +0300

    Videos 120-128 are complete

--- build/index.js
+++ build/index.js
@@ -1,18 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const MatchResult_1 = require("./MatchResult");
 const CsvFileReader_1 = require("./CsvFileReader");
-const reader = new CsvFileReader_1.CsvFileReader("football.csv");
-reader.read();
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
-    if ((match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
-        (match[2] === "Man United" && match[5] === MatchResult.AwayWin)) {
+for (let match of matchReader.matches) {
+    if ((match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) ||
+        (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin)) {
         manUnitedWins++;
     }
 }
